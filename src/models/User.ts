37ddae67b6eadfbed7c884445e013ac67a09ae21
CREDO23 <thierrybakera12@gmail.com>
2023-01-29 19:37:33 +0200

    feat : add test for login endpoint

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,19 @@
 import sequelize from "../configs/database";
 import { DataTypes, Model } from "sequelize";
-import IUser from "../types/IUser";
 
-export default class User extends Model<IUser> {
+export default class User extends Model {
+  declare id: typeof DataTypes.UUID;
   declare username: string;
-  declare id: string;
-  declare email: string;
+  declare firstname?: string;
+  declare lastname?: string;
   declare password: string;
-  declare firstname: string;
+  declare email: string;
+  declare phoneNumber?: string;
+  declare proffession: string;
+  declare description: string;
+  declare gender: string;
+  declare avatar?: string;
+  declare isOnline?: boolean;
 }
 
 User.init(
