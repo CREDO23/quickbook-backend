b4976918eff89ebfb535f6e8db8f94984a058894
CREDO23 <thierrybakera12@gmail.com>
2023-02-01 14:44:31 +0200

    add testing in gh

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,11 @@ const app: App = new App();
 const PORT = process.env.PORT || 3000;
 
 app.init().then(() => {
-  app.server.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Listening on ${PORT}`);
-  });
+  process.env.NODE_ENV !== "test" &&
+    app.server.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Listening on ${PORT}`);
+    });
 });
 
 export default app;
